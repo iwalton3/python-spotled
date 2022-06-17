95a0f57b1e636fd5a92276e9532877b924b92d6d
Ian Walton <ian@iwalton.com>
2022-06-17 11:41:07 -0400

    Add pypi package.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='spotled',
+    version='1.0.0',
+    author="Ian Walton",
+    author_email="ian@iwalton.com",
+    description="Allows control of SPOTLED bluetooth led displays via Python. (Unofficial)",
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/iwalton3/python-spotled",
+    py_modules=['spotled'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=['gattlib'],
+    include_package_data=True
+)
