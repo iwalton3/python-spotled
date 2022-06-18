bb5f4210afc4cc7268a39010348b0a0d1d8f38ba
Ian Walton <ian@iwalton.com>
2022-06-17 20:55:07 -0400

    Up version to 1.1.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='spotled',
-    version='1.0.1',
+    version='1.1.0',
     author="Ian Walton",
     author_email="ian@iwalton.com",
     description="Allows control of SPOTLED bluetooth led displays via Python. (Unofficial)",
